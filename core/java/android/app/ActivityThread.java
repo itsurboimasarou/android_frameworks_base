f562584e422048e2d318b194db5e6245f9362b46
Wei Xiaodong <wei.xiaodong4@zte.com.cn>
2022-07-18 17:04:34 +0800

    Fix bug provider has been publised but the system still waits 20s
    - scenario
    
    1. We add log to debug, find provider has been publised but the
    system still implement wait;
    
    2. In the acquireProvider function, the provider published between
    AMS.getContentProvider and if judgment, the system still implement
    wait. The system won't get notify while waiting, because the provider
    has been published. So, the system has to wait 20s.
    
    - problem
    
    The provider has been publised, we have to wait 20s to get provider.
    This will cause some problems, like ANR and so on.
    
    Change-Id: Ic3d4f44262380a2b9b554ab370a976a97ccf463b
    Signed-off-by: minaripenguin <alexfinhart@gmail.com>

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -439,6 +439,8 @@ public final class ActivityThread extends ClientTransactionHandler
     // An executor that performs multi-step transactions.
     private final TransactionExecutor mTransactionExecutor = new TransactionExecutor(this);
 
+    private boolean mIsProviderStatus = false;
+
     @UnsupportedAppUsage(maxTargetSdk = Build.VERSION_CODES.P, trackingBug = 115609023)
     private final ResourcesManager mResourcesManager;
 
@@ -1857,6 +1859,7 @@ public final class ActivityThread extends ClientTransactionHandler
                 synchronized (key.mLock) {
                     key.mHolder = holder;
                     key.mLock.notifyAll();
+                    mIsProviderStatus = true;
                 }
             }
         }
@@ -6992,6 +6995,7 @@ public final class ActivityThread extends ClientTransactionHandler
     public final IContentProvider acquireProvider(
             Context c, String auth, int userId, boolean stable) {
         final IContentProvider provider = acquireExistingProvider(c, auth, userId, stable);
+        mIsProviderStatus = false;
         if (provider != null) {
             return provider;
         }
@@ -7012,7 +7016,7 @@ public final class ActivityThread extends ClientTransactionHandler
                 // local, we'll need to wait for the publishing of the provider.
                 if (holder != null && holder.provider == null && !holder.mLocal) {
                     synchronized (key.mLock) {
-                        if (key.mHolder != null) {
+                        if (mIsProviderStatus && key.mHolder != null) {
                             if (DEBUG_PROVIDER) {
                                 Slog.i(TAG, "already received provider: " + auth);
                             }
